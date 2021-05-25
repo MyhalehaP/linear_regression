a3d110e8215027f0e36d075b63ac05399aa40de5
Peter <migalega.p@gmail.com>
2021-05-25 17:37:46 +0300

    message

--- main.py
+++ main.py
@@ -7,12 +7,44 @@ from sklearn.model_selection import train_test_split
 
 data = panda.read_csv('train.csv')
 
-df = panda.DataFrame(data,columns=['OverallQual','SalePrice'])
+plot.rcParams['figure.figsize'] = (8,8)
+#
+# plot.scatter(data["Foundation"],data["SalePrice"])
+#
+# plot.xticks(rotation=60)
+# plot.xlabel("Тип основи")
+# plot.ylabel("Ціна")
+#
+#
+#
+# plot.show()
+# print(data.info)
+
+df = panda.DataFrame(data,columns=['HeatingQC','SalePrice'])
 
 test_data = panda.read_csv('test.csv')
 test = panda.DataFrame(test_data,columns=['Id','OverallQual'])
 
-x_train, x_test, y_train, y_test = train_test_split(df['OverallQual'], df['SalePrice'], test_size=0.2)
+dummies = panda.get_dummies(df['HeatingQC'])
+print(dummies)
+clms = []
+k = 10
+for i in range(0,len(dummies.columns)):
+    clms.append(k)
+    k+=10
+
+
+values = []
+
+for index,row in dummies.iterrows():
+    for i in range(0,len(row)):
+        if row.values[i] == 1:
+            values.append(clms[i])
+            break
+
+values = np.array(values).reshape(-1,1)
+
+x_train, x_test, y_train, y_test = train_test_split(values, df['SalePrice'], test_size=0.2)
 
 x_train = np.array(x_train).reshape(-1, 1)
 x_test = np.array(x_test).reshape(-1, 1)
@@ -30,20 +62,20 @@ print(r2_score(y_test, price_predict))
 plot.scatter(x_test,y_test,1)
 plot.plot(x_test,price_predict,color='red',linewidth=1)
 
-plot.xlabel('Overall Quality')
+plot.xlabel('Якість матеріалів')
 plot.ylabel('Sale Price')
 plot.show()
 
-#first change in test_3
-#second change in test_3
-
-output = []
-
-for index,row in test.iterrows():
-    quality = np.array(row['OverallQual']).reshape(-1,1)
-    predict = ln_regression.predict(quality)
-    output.append([row['Id'],predict[0][0]])
-
-output_df = panda.DataFrame(output,columns=['Id','SalePrice'])
-output_df.to_csv("prediction.csv",index=False)
-
+# #first change in test_3
+# #second change in test_3
+#
+# output = []
+#
+# for index,row in test.iterrows():
+#     quality = np.array(row['OverallQual']).reshape(-1,1)
+#     predict = ln_regression.predict(quality)
+#     output.append([row['Id'],predict[0][0]])
+#
+# output_df = panda.DataFrame(output,columns=['Id','SalePrice'])
+# output_df.to_csv("prediction.csv",index=False)
+#
